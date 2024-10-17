5cb184b0d7b5e1d982ac7e6a5afff531fb40b0ac
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-10-17 12:10:49 +0200

    Klar

--- store/Program.cs
+++ store/Program.cs
@@ -5,15 +5,16 @@ while (pengar > 0)
   Console.WriteLine($"Du har ${pengar}.");
 
 
-  Console.WriteLine("Vilket par skor vill du köpa?");
+  Console.WriteLine("Vad skulle du vilja köpa?");
   Console.WriteLine("1. Balenciaga Tracks ($1000)");
   Console.WriteLine("2. Jordan 4 Retro Black Cats ($880)");
   Console.WriteLine("3. Vetements Black New Rock Edition Platforms ($1280)");
-  Console.WriteLine("4. Nike Air Force 1 Low ($110)");
+  Console.WriteLine("4. Nike Air Force 1 Low ($100)");
   Console.WriteLine("5. Rick Owens FW24 Porterville Jumbo Laced Sneakers ($1200)");
+  Console.WriteLine("6. Klubba ($1)");
 
   string vadKopa = "";
-  while (vadKopa != "1" && vadKopa != "2" && vadKopa != "3" && vadKopa != "4" && vadKopa != "5")
+  while (vadKopa != "1" && vadKopa != "2" && vadKopa != "3" && vadKopa != "4" && vadKopa != "5" && vadKopa != "6")
   {
     vadKopa = Console.ReadLine();
   }
@@ -35,11 +36,15 @@ while (pengar > 0)
   }
   else if (vadKopa == "4")
   {
-    prisPerForemal = 110;
+    prisPerForemal = 100;
   }
   else if (vadKopa == "5")
   {
     prisPerForemal = 1200;
+  }
+  else if (vadKopa == "6")
+  {
+    prisPerForemal = 1;
   }
 
   Console.WriteLine("Hur många vill du köpa?");
